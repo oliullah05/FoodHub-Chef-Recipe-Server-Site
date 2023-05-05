2f8d76f0d788595c16564ed6568a92c254a60367
oliullah05 <mdolihasan@gmail.com>
2023-05-05 09:51:27 +0600

    cors and cors setup done

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const app = express();
-
-
 const cors = require("cors");
+const port = process.env.PORT || 5000;
 app.use(cors());
 
-
-const port = process.env.PORT || 5000;
-
-
+const chefData = require("./chefData.json");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/allchefdata", (req, res) => {
+  res.send(chefData);
+});
 
+app.get("/allchefdata/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedChef = chefData.find((chef) => chef.id == id);
+  res.send(selectedChef);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
